4bd9f5031117b7b635217ccb0ac00cad57b2f7c5
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-24 13:27:15 +0100

    ok

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -65,6 +65,7 @@ class BlogsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
+      
     end
 
     # Only allow a list of trusted parameters through.
