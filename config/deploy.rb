18c3ba0eb3e8c7424b85f135cac3c23dadc19527
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-24 13:22:32 +0100

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.6.5' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-2.7.1' # Edit this if you are using MRI Ruby
 
 set :log_level, :info
 namespace :deploy do
