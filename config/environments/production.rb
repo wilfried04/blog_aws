c5ad3ee66236c42a63b8fcd644892a523733ed91
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-30 13:51:31 +0100

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  #config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   config.eager_load = true
 
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+#config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
