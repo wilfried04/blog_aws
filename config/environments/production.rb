123f3d70d2accfd09d593b0f2d746ac8365b3be8
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-30 11:18:32 +0100

    ooook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
